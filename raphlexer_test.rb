787b21f15d473b96daedb74b46e3f1aa38e31a37
Raphael Villas <raphael.villas@gmail.com>
2015-03-17 23:45:36 -0500

    Initial commit of test

---
+++ raphlexer_test.rb
@@ -0,0 +1,60 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class RaphLexer
+
+  def tokenize code
+    tokens = []
+
+    if number = code[/\A([0-9])+/,1]
+      tokens << [:NUMBER, number.to_i]
+      # todo: increment current location with size of each character being analyzed
+    elsif string = code[/\A"([A-Za-z]+)"/,1]
+      tokens << [:STRING, string]
+    elsif identifier = code[/([A-Za-z]+)/, 1]
+      tokens << [:IDENTIFIER, identifier]
+    end
+  end
+end
+
+class TestRaphLexer < Minitest::Test
+  describe RaphLexer do
+    before do
+      @lexer = RaphLexer.new
+    end
+
+    it 'tokenizes a number' do
+      assert_equal [[:NUMBER, 1]], RaphLexer.new.tokenize("1")
+    end
+
+    it 'tokenizes a string' do
+      assert_equal [[:STRING, "something"]], RaphLexer.new.tokenize('"something"')
+    end
+
+    it 'tokenizes an identifier' do
+      assert_equal [[:IDENTIFIER, "name"]], RaphLexer.new.tokenize('name')
+    end
+  end
+end
+
+# def test_number
+#   assert_equal [[:NUMBER, 1]], Lexer.new.tokenize("1")
+# end
+
+# def test_string
+#   assert_equal [[:STRING, "hi"]], Lexer.new.tokenize('"hi"')
+# end
+
+# def test_identifier
+#   assert_equal [[:IDENTIFIER, "name"]], Lexer.new.tokenize('name')
+# end
+
+# def test_constant
+#   assert_equal [[:CONSTANT, "Name"]], Lexer.new.tokenize('Name')
+# end
+
+# def test_operator
+#   assert_equal [["+", "+"]], Lexer.new.tokenize('+')
+#   assert_equal [["||", "||"]], Lexer.new.tokenize('||')
+# end
